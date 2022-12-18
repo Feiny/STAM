1868799d3e2923a2d2ef6602000b955cec99bd4e
Fei Ni <nifei1998@gmail.com>
2022-12-18 11:19:30 +0800

    Update filter_mutation.pl

--- filter_mutation.pl
+++ filter_mutation.pl
@@ -10,11 +10,11 @@ while(<>){
         my $mis=$line=~s/\./\./g;
         my $unknown=$line=~s/-/-/g;
         my $cluster=$line=~s/,/,/g;
-        my $mutants=$line=~s/(\w)/$1/g;
-        #print "$mis:$unknown:$cluster:$mutants\n";
+        my $mutantions=$line=~s/(\w)/$1/g;
+        #print "$mis:$unknown:$cluster:$mutations\n";
         next if $mis>=3;
-        next if $mutants<1;
-        #next unless $mutants==1;
-        print "$_\t$mis:$unknown:$cluster:$mutants\n";
+        next if $mutantions<1;
+        #next unless $mutations==1;
+        print "$_\t$mis:$unknown:$cluster:$mutations\n";
     }
 }
