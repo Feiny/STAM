6b7ac203c595f22d76e3b610fc383ecc971c98f8
Fei Ni <nifei1998@gmail.com>
2022-12-27 16:14:55 +0800

    Update fish_fasta_unmasked.pl

--- Scripts/fish_fasta_unmasked.pl
+++ Scripts/fish_fasta_unmasked.pl
@@ -1,1 +1,19 @@
 
+#!/usr/bin/perl -w
+my %id;
+open(IN1,"<$ARGV[0]") or die;
+while(<IN1>){
+  my @line=split;
+  $id{$line[0]}=1;
+}
+close IN1;
+
+$/=">";
+open(IN2,"<$ARGV[1]") or die;
+while(<IN2>){
+  s/>//;
+  next unless my ($id, $seq)=split(/\n/, $_, 2);
+  my @id=split(/\s/,$id);
+  print ">$id$seq" unless exists ($id{$id[0]});
+}
+close IN2;
