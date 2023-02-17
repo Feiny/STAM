0cfbc2e5365a4a4290f765fb12efd54438accbec
Feiny <nifei1998@gmail.com>
2023-02-17 17:30:14 +0800

    Update select_mutation2.pl

--- Scripts/select_mutation2.pl
+++ Scripts/select_mutation2.pl
@@ -1,7 +1,11 @@
 
-#!/usr/bin/perl -w
+#!/usr/bin/perl
+use strict;
+use warnings;
+
 my %hash;
 my @array;
+
 while(<>){
     my @line=split;
     $hash{$line[0]} .= $_;
@@ -10,56 +14,53 @@ while(<>){
 
 my %uniq;
 my @uniq=grep{++$uniq{$_}<2}@array;
+
 foreach my $key (@uniq){
-    if($key=~/^#/){
+    if($key =~ /^#/){
         print "$hash{$key}";
     }else{
-        my $i=0;
         my @data;
         my @input=split(/\n/, $hash{$key});
         foreach(@input){
             chomp;
             my @line=split/\t/,$_;
             for my $k(0 .. $#line){
-                $data[$k][$i]=$line[$k];
+                $data[$k][$.-1]=$line[$k]; 
             }
-            $i++;
         }
-        my $j=0;
-        my @trans;
-        for(my $n=0;$n<=$#data;$n++){
-             my @line = @{$data[$n]};
-             if($n<=3){
-                  for my $k(0 .. $#line){
-                      $trans[$k][$j]=$line[$k];
-                  }
-                  $j++;
-             }elsif($n>3 and $n<$#data){
-                  my $line=join "\t", @line;
-                  my $count=$line=~s/(\w)/$1/g; #AT
-                  if($count>1){   ## 1 or 2?
-                      foreach(@line){
-                          $_=~s/\w/,/g
-                      }
-                      for my $k(0 .. $#line){
-                          $trans[$k][$j]=$line[$k];
-                      }
-                      $j++;
-                  }else{
-                      for my $k(0 .. $#line){
-                          $trans[$k][$j]=$line[$k];
-                      }
-                      $j++;
-                  }
-             }elsif($n==$#data){
-                  for my $k(0 .. $#line){
-                      $trans[$k][$j]=$line[$k];
-                  }
-                  $j++;
-            }
 
+        my @trans;
+        my $j=0;
+        for my $n (0..$#data){
+            my @line = @{$data[$n]};
+            if($n<=3){
+                for my $k(0 .. $#line){
+                    $trans[$k][$j]=$line[$k];
+                }
+                $j++;
+            }elsif($n>3 and $n<$#data){
+                my $count= $line[$n]=~s/(\w)/$1/g; #AT
+                if($count>1){   ## 1 or 2?
+                    $_=~s/\w/,/g for @line;
+                    for my $k(0 .. $#line){
+                        $trans[$k][$j]=$line[$k];
+                    }
+                    $j++;
+                }else{
+                    for my $k(0 .. $#line){
+                        $trans[$k][$j]=$line[$k];
+                    }
+                    $j++;
+                }
+            }elsif($n==$#data){
+                for my $k(0 .. $#line){
+                    $trans[$k][$j]=$line[$k];
+                }
+                $j++;
+            }
          }
-        foreach (@trans){
+
+        for (@trans){
             print join("\t", @{$_}), "\n";
         }
     }
