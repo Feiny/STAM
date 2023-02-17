8a4c3ac0be4bbca19bf7eca1e59ff382c411ed78
Feiny <nifei1998@gmail.com>
2023-02-17 17:23:19 +0800

    Update select_homo.pl

--- Scripts/select_homo.pl
+++ Scripts/select_homo.pl
@@ -1,27 +1,29 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
 
-#!/usr/bin/perl -w
-while(<>){
-    if (/\A#/){
-        next if /\A##/;
+while (<>) {
+    if (/^#/) {
+        next if /^##/;
         s/_.*?\t/\t/g;
         my @head = split;
         my $print = join("\t", @head[0,1,3,4,9..$#head]);
         print "$print\n";
-    }else{
+    } else {
         my @line = split;
         next unless $line[6] eq "PASS";
-        for (my $i=9; $i<=$#line; $i++){
+        for (my $i=9; $i<=$#line; $i++) {
             $line[$i] =~ s/\|/\//;
             my @genotype = split(/:/, $line[$i]);
-            $line[$i] = "./." if (!$genotype[2]);
-            if ($genotype[0] =~ /0\/0|1\/1/ and $genotype[2] > 4){
+            $line[$i] = "./." unless $genotype[2];
+            if ($genotype[0] =~ /0\/0|1\/1/ and $genotype[2] > 4) {
                 $line[$i] = $genotype[0];
-        }else{
-            $line[$i] = "./.";
+            } else {
+                $line[$i] = "./.";
+            }
         }
-    }
-    my $print = join("\t", @line[0,1,3,4,9..$#line]);
-    my $info = join("\t", @line[9..$#line]);
-    print "$print\n" if ($info =~ /0\/0/ and $info =~ /1\/1/);
+        my $print = join("\t", @line[0,1,3,4,9..$#line]);
+        my $info = join("\t", @line[9..15]); 
+        print "$print\n" if $info =~ /0\/0/ and $info =~ /1\/1/;
     }
 }
