bf7ce662004a630594e9552af8dfee13d2370d9d
Feiny <nifei1998@gmail.com>
2024-02-15 13:42:11 +0800

    Update filter_mutation.pl

--- Scripts/filter_mutation.pl
+++ Scripts/filter_mutation.pl
@@ -1,21 +1,66 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
 
-#!/usr/bin/perl -w
+my %hash;
+my @array;
+
 while(<>){
-    if(/\A#/){
-        print;
+    my @line=split;
+    $hash{$line[0]} .= $_;
+    push @array, $line[0];
+}
+
+my %uniq;
+my @uniq=grep{++$uniq{$_}<2}@array;
+
+foreach my $key (@uniq){
+    if($key =~ /^#/){
+        print "$hash{$key}";
     }else{
-        chomp;
-        my @line=split;
-        #next unless $line[11]=~/\w/;
-        my $line= join "\t", @line[4..$#line];
-        my $mis=$line=~s/\./\./g;
-        my $unknown=$line=~s/-/-/g;
-        my $cluster=$line=~s/,/,/g;
-        my $mutantions=$line=~s/(\w)/$1/g;
-        #print "$mis:$unknown:$cluster:$mutations\n";
-        next if $mis>=3;
-        next if $mutantions<1;
-        #next unless $mutations==1;
-        print "$_\t$mis:$unknown:$cluster:$mutations\n";
+        my @data;
+        my @input=split(/\n/, $hash{$key});
+        foreach(@input){
+            chomp;
+            my @line=split/\t/,$_;
+            for my $k(0 .. $#line){
+                $data[$k][$.-1]=$line[$k]; 
+            }
+        }
+
+        my @trans;
+        my $j=0;
+        for my $n (0..$#data){
+            my @line = @{$data[$n]};
+            if($n<=3){
+                for my $k(0 .. $#line){
+                    $trans[$k][$j]=$line[$k];
+                }
+                $j++;
+            }elsif($n>3 and $n<$#data){
+                my $count= $line[$n]=~s/(\w)/$1/g; #AT
+                if($count>1){   ## 1 or 2?
+                    $_=~s/\w/,/g for @line;
+                    for my $k(0 .. $#line){
+                        $trans[$k][$j]=$line[$k];
+                    }
+                    $j++;
+                }else{
+                    for my $k(0 .. $#line){
+                        $trans[$k][$j]=$line[$k];
+                    }
+                    $j++;
+                }
+            }elsif($n==$#data){
+                for my $k(0 .. $#line){
+                    $trans[$k][$j]=$line[$k];
+                }
+                $j++;
+            }
+         }
+
+        for (@trans){
+            print join("\t", @{$_}), "\n";
+        }
     }
 }
