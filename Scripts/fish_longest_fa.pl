9b1f4d52246452d77a25c338ac3a92a8cd8d1670
Feiny <nifei1998@gmail.com>
2024-03-05 18:15:49 +0800

    Update fish_longest_fa.pl

--- Scripts/fish_longest_fa.pl
+++ Scripts/fish_longest_fa.pl
@@ -1,20 +1,20 @@
 #!/usr/bin/perl -w
-# Usage: perl $0 cds.fa > output.fa
 use strict;
 
 my (%h1, %h2);
 $/ = "\>";
-open(my $in1, "-|", "gzip -dc $ARGV[0]") or die "Cannot open input file: $!";
+open(my $in1, $ARGV[0]) or die "Cannot open input file: $!";
 
 while (<$in1>) {
     chomp;
+    next unless my ($id, @fastq) = split /\n/;
     my @id = split(/\|/, $id); # To modify the regular expression based on the sequence name
+#    my $tran = $id[0];
     my $gene = $id[0];
     $gene =~ s/\.\d+\z//;
     push @{$h2{$id}}, @fastq;
     $h1{$gene} ||= $id;
-    $h1{$gene} = $id if length(join("", @{$h2{$h1{$gene}}}})) < length(join("", @fastq));
-
+    $h1{$gene} = $id if length(join("", @{$h2{$h1{$gene}}})) < length(join("", @fastq));
 }
 
 $/ = "\n";
